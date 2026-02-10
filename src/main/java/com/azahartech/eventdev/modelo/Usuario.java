2a2dc99c8dbe992e0417b7b86c312b4b734d854f
Pau Pasalodos <paupasalodos@gmail.com>
2026-02-10 19:26:52 +0100

    getters, seters, constructor

---
+++ src/main/java/com/azahartech/eventdev/modelo/Usuario.java
@@ -0,0 +1,50 @@
+package com.azahartech.eventdev.modelo;
+
+public class Usuario {
+    private String nombre;
+    private String email;
+    private String telefono;
+    private boolean esVip;
+
+    public Usuario(String nombre, String email, String telefono, boolean esVip) {
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.esVip = esVip;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isEsVip() {
+        return esVip;
+    }
+
+    public void setEsVip(boolean esVip) {
+        this.esVip = esVip;
+    }
+
+
+
+}
